ba3bf31d753e3f9559b829648a105e65fcacb6b9
Arin Upadhyay <arinupadhyay.cs@gmail.com>
2025-12-21 00:14:35 +0100

    lib

--- src/main.c
+++ src/main.c
@@ -1,27 +1,27 @@
 #include "rylr_efpix.c"
 #include <stdio.h>
 int main() {
-    Identity other={0};
-    generate_identity(&node);
-    generate_identity(&other);
-    Send send={0};
-    uint8_t packet[PACKET_SIZE]={0};
+    LIBEFPIX_Identity other={0};
+    LIBEFPIX_generate_identity(&node);
+    LIBEFPIX_generate_identity(&other);
+    LIBEFPIX_Send send={0};
+    uint8_t packet[LIBEFPIX_PACKET_SIZE]={0};
     send.identity=node;
     alias_copy(send.my_alias, "DSPTCH");
     memcpy(send.receiver_kx_public_key, other.kx_public_key, 32);
     get_timestamp(send.timestamp);
     memcpy(send.message, "HELLO", 5);
-    encode(send, packet);
+    LIBEFPIX_encode(send, packet);
 
     char buffer[240];
-    bintob85(buffer, packet, PACKET_SIZE);
+    bintob85(buffer, packet, LIBEFPIX_PACKET_SIZE);
     printf("%s\n", buffer);
     printf("%ld\n", strlen(buffer));
-    printf("%d\n", PACKET_SIZE);
+    printf("%d\n", LIBEFPIX_PACKET_SIZE);
 
     b85tobin(packet, buffer);
-    Recv recv={0};
-    printf("%d\n", decode(packet, other, &recv, hash_check_and_relay, get_contact_from_alias, get_timestamp, get_age));
+    LIBEFPIX_Recv recv={0};
+    printf("%d\n", LIBEFPIX_decode(packet, other, &recv, hash_check_and_relay, get_contact_from_alias, get_timestamp, get_age));
     printf("%s\n", recv.message);
 
     bintob85(buffer, node.kx_public_key, 32);
--- src/rylr_efpix.c
+++ src/rylr_efpix.c
@@ -1,3 +1,4 @@
+#include "libefpix_config.h"
 #include "../lib/libefpix.h"
 #include "../lib/base85.h"
 #include "rylr.c"
@@ -10,47 +11,47 @@
 #define MAX_CONTACTS 10
 #define MAX_HASHES 100
 //=============================================================================================
-Identity node;
-uint8_t stored_hashes[MAX_HASHES][HASH_SIZE];
+LIBEFPIX_Identity node;
+uint8_t stored_hashes[MAX_HASHES][LIBEFPIX_HASH_SIZE];
 int hash_count = 0;
-Contact contacts[MAX_CONTACTS];
+LIBEFPIX_Contact contacts[MAX_CONTACTS];
 int contact_count = 0;
 int ignore;
 //=============================================================================================
-bool hash_check_and_relay(uint8_t hash[HASH_SIZE], uint8_t packet[PACKET_SIZE]) {
-    for (int i = 0; i < hash_count; i++) {if (memcmp(stored_hashes[i], hash, HASH_SIZE) == 0) return false;}
-    if (hash_count < MAX_HASHES) {memcpy(stored_hashes[hash_count], hash, HASH_SIZE); hash_count=hash_count+1;}
+bool hash_check_and_relay(uint8_t hash[LIBEFPIX_HASH_SIZE], uint8_t packet[LIBEFPIX_PACKET_SIZE]) {
+    for (int i = 0; i < hash_count; i++) {if (memcmp(stored_hashes[i], hash, LIBEFPIX_HASH_SIZE) == 0) return false;}
+    if (hash_count < MAX_HASHES) {memcpy(stored_hashes[hash_count], hash, LIBEFPIX_HASH_SIZE); hash_count=hash_count+1;}
     ignore=packet[0];
     return true;
 }
-bool get_contact_from_alias(uint8_t alias[ALIAS_SIZE], Contact* contact) {
-    for (int i = 0; i < contact_count; i++) {if (memcmp(contacts[i].their_alias, alias, ALIAS_SIZE) == 0) {memcpy(contact, &contacts[i], sizeof(Contact)); return true;}}
+bool get_contact_from_alias(uint8_t alias[LIBEFPIX_ALIAS_SIZE], LIBEFPIX_Contact* contact) {
+    for (int i = 0; i < contact_count; i++) {if (memcmp(contacts[i].their_alias, alias, LIBEFPIX_ALIAS_SIZE) == 0) {memcpy(contact, &contacts[i], sizeof(LIBEFPIX_Contact)); return true;}}
     return false;
 }
-void get_timestamp(uint8_t timestamp[TIMESTAMP_SIZE]) {
+void get_timestamp(uint8_t timestamp[LIBEFPIX_TIMESTAMP_SIZE]) {
     uint32_t now = (uint32_t)(time(NULL)-1577836800);
-    memcpy(timestamp, &now, TIMESTAMP_SIZE);
+    memcpy(timestamp, &now, LIBEFPIX_TIMESTAMP_SIZE);
 }
-uint32_t get_age(uint8_t recv_time[TIMESTAMP_SIZE], uint8_t send_time[TIMESTAMP_SIZE]) {
+uint32_t get_age(uint8_t recv_time[LIBEFPIX_TIMESTAMP_SIZE], uint8_t send_time[LIBEFPIX_TIMESTAMP_SIZE]) {
     uint64_t recv_ts, send_ts;
-    memcpy(&recv_ts, recv_time, TIMESTAMP_SIZE);
-    memcpy(&send_ts, send_time, TIMESTAMP_SIZE);
+    memcpy(&recv_ts, recv_time, LIBEFPIX_TIMESTAMP_SIZE);
+    memcpy(&send_ts, send_time, LIBEFPIX_TIMESTAMP_SIZE);
     if (recv_ts >= send_ts) {return (uint32_t)(recv_ts - send_ts);}
     else {return UINT32_MAX;}
 }
-void add_contact(Contact contacts[MAX_CONTACTS], int* contact_count, const char* their_alias_str, uint8_t kx_public_key[32], uint8_t sign_public_key[32], const char* my_alias_str) {
+void add_contact(LIBEFPIX_Contact contacts[MAX_CONTACTS], int* contact_count, const char* their_alias_str, uint8_t kx_public_key[32], uint8_t sign_public_key[32], const char* my_alias_str) {
     if (*contact_count < 10) {
-        memset(contacts[*contact_count].their_alias, 0, ALIAS_SIZE);
-        memset(contacts[*contact_count].my_alias, 0, ALIAS_SIZE);
-        strncpy((char*)contacts[*contact_count].their_alias, their_alias_str, ALIAS_SIZE - 1);
-        strncpy((char*)contacts[*contact_count].my_alias, my_alias_str, ALIAS_SIZE - 1);
+        memset(contacts[*contact_count].their_alias, 0, LIBEFPIX_ALIAS_SIZE);
+        memset(contacts[*contact_count].my_alias, 0, LIBEFPIX_ALIAS_SIZE);
+        strncpy((char*)contacts[*contact_count].their_alias, their_alias_str, LIBEFPIX_ALIAS_SIZE - 1);
+        strncpy((char*)contacts[*contact_count].my_alias, my_alias_str, LIBEFPIX_ALIAS_SIZE - 1);
         memcpy(contacts[*contact_count].kx_public_key, kx_public_key, 32);
         memcpy(contacts[*contact_count].sign_public_key, sign_public_key, 32);
         *contact_count=*contact_count+1;
     }
 }
-void alias_copy(uint8_t dest[ALIAS_SIZE], const char* src) {
-    memset(dest, 0, ALIAS_SIZE);
-    strncpy((char*)dest, src, ALIAS_SIZE - 1);
-    dest[ALIAS_SIZE - 1] = '\0';
+void alias_copy(uint8_t dest[LIBEFPIX_ALIAS_SIZE], const char* src) {
+    memset(dest, 0, LIBEFPIX_ALIAS_SIZE);
+    strncpy((char*)dest, src, LIBEFPIX_ALIAS_SIZE - 1);
+    dest[LIBEFPIX_ALIAS_SIZE - 1] = '\0';
 }
